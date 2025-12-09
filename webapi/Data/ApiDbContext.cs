6f07cbfb8431eb8c4b6dfa6a4f32524e00b78d97
SatNarayanSah <satnarayan5166@gmail.com>
2025-12-10 01:39:17 +0545

    db context added

---
+++ webapi/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi.Modals.Domain;
+
+namespace webapi.Data
+{
+    public class ApiDbContext:DbContext
+    {
+        public ApiDbContext(DbContextOptions dbContextoptions):base(dbContextoptions)
+        {
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
